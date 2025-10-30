dd972a47e13a507db92b53abc0aceab267e88c14
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-30 20:35:34 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./Components/Button";
+import Header from "./Components/Header";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+      <Header />
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
