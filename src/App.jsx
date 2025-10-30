77e4d1c6dfc32df8614f8c4659143fb4c15aec0d
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-30 23:30:36 +0100

    WIP: hero section layout in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Button from "./Components/Button";
 import Header from "./Components/Header";
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
       <Header />
+      <Hero />
       <ButtonGradient />
     </>
   );
