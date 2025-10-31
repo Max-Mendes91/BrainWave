87f73053b15f2edf0f10307b7d01efbee6415d86
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-31 15:25:00 +0100

    Add Colaboration component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Header from "./Components/Header";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Hero from "./Components/Hero";
 import Benefits from "./Components/Benefits";
+import Colaboration from "./Components/Colaboration";
 
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
-      <Header />
-      <Hero />
-      <Benefits />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Colaboration />
+      </div>
+
       <ButtonGradient />
     </>
   );
